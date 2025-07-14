7f0910e5f32e38d32552a5d30a22c8d16dcda31a
K3NZORION <k3nzorion@gmail.com>
2025-07-14 21:57:45 +0200

    Submitioin bug partially fixed
    Submition bug is fixed on the Login modal.

--- obj/Debug/net8.0/Tujyane.AssemblyInfo.cs
+++ obj/Debug/net8.0/Tujyane.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tujyane")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a3cfbc471931b53f1e89b763e68b155e6f390e2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac7cd1222099c27e31f2f3c4e1de9c08c33be5c7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tujyane")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tujyane")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
